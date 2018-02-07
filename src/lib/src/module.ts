9cee26c433d133a2dc3940a623ba41d8865fa67c
Carlos Schneider <carlosaschjr@gmail.com>
2018-02-07 15:45:53 -0200

    Try to fix the AfterViewInit Life Cycle

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -2,9 +2,11 @@ import { CheckContainerDirective } from './directives/check-container.directive'
 import { CheckOneDirective } from './directives/check-one.directive';
 import { CheckAllDirective } from './directives/check-all.directive';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
+  imports: [FormsModule],
   declarations: [
     CheckAllDirective,
     CheckContainerDirective,
